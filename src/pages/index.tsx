0d21b31b908efcd23b9cff0872897290f79edfd3
vchho <12765624+vchho@users.noreply.github.com>
2022-10-24 22:57:46 -0700

    Tailwind added and changed file structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center">
+      tailwind works
+    </h1>
+  );
+};
+
+export default Home;
