abc3421390c4399c0b19cda03a2ab033dec950a8
vchho <12765624+vchho@users.noreply.github.com>
2022-10-25 22:14:08 -0700

    testing prisma data fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
-import type { NextPage } from "next";
+import type { InferGetServerSidePropsType, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { prisma } from "../utils/prisma";
 
-const Home: NextPage = () => {
+export async function getServerSideProps() {
+  const threads = await prisma.thread.findMany();
+
+  console.log("threads", threads);
+
+  return {
+    props: {
+      threads: JSON.parse(JSON.stringify(threads)),
+    },
+  };
+}
+
+const Home = (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) => {
+  console.log(props.threads);
   return (
     <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center">
       tailwind works
